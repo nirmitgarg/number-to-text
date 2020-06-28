0e3711c0ed2fad8260dfb98382afc8ad9f7bc492
Nirmit Garg <garg.nirmit@hotmail.com>
2020-06-28 14:10:52 +1000

    Initial add

---
+++ NumberToText/Extension.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NumberToText
+{
+    public static class Extension
+    {
+        static IDictionary<int, string> numberToTextMapping = new Dictionary<int, string>
+            {
+                { 0, " zero " } ,
+                { 1,  " one "  },
+                { 2,  " two "  },
+                { 3,  " three "  },
+                { 4,  " four "  },
+                { 5,  " five "  },
+                { 6,  " six "  },
+                { 7,  " seven "  },
+                { 8,  " eight "  },
+                { 9,  " nine "  },
+                { 10,  " ten "  },
+                { 11,  " eleven "  },
+                { 12,  " twelve "  },
+                { 13,  " thirteen "  },
+                { 14,  " fourteen "  },
+                { 15,  " fifteen "  },
+                { 16,  " sixteen "  },
+                { 17,  " seventeen "  },
+                { 18,  " eighteen "  },
+                { 19,  " nineteen "  },
+                { 20,  " twenty "  },
+                { 30,  " thirty "  },
+                { 40,  " forty "  },
+                { 50,  " fifty "  },
+                { 60,  " sixty "  },
+                { 70,  " seventy "  },
+                { 80,  " eighty "  },
+                { 90,  " ninety "  },
+                { 100, " hundred " },
+                { 1000, " thousand " },
+                { 1000000, " million " }
+        };
+
+        public static string NumberToText(this int number)
+        {
+            var words = string.Empty;
+
+            List<int> divideByList = new List<int> { 1000000, 1000, 100 };
+
+            foreach (var divideBy in divideByList)
+            {
+                GetRemainderAndText(divideBy, ref number, ref words);
+            }
+
+            if (number > 0)
+            {
+                if (words != "")
+                    words += " and ";
+
+                if (number < 20)
+                    words += numberToTextMapping[number];
+                else
+                {
+                    words += numberToTextMapping[(number / 10) * 10];
+
+                    if ((number % 10) > 0)
+                        words += numberToTextMapping[number % 10];
+                }
+            }
+            return words;
+        }
+
+        private static void GetRemainderAndText(int divideBy, ref int number, ref string words)
+        {
+            if ((number / divideBy) > 0)
+            {
+                words += NumberToText(number / divideBy) + numberToTextMapping[divideBy];
+                number %= divideBy;
+            }
+        }
+    }
+}
---
+++ NumberToText/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace NumberToText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number: ");
+            string input = Console.ReadLine();
+            bool isAmount;
+
+            isAmount = double.TryParse(input, out double amount);
+            var regex = @"^\d+(?:\.\d{0,2})?$";
+
+            var match = Regex.Match(input, regex);
+
+            if (!match.Success || !isAmount || amount <= 0)            
+                Console.WriteLine("Input is not a valid currency.");            
+            else
+            {
+                var amountArray = input.Split('.');
+
+                int dollar = Convert.ToInt32(input.Split('.')[0]);
+                string output = dollar.NumberToText() + " dollars ";
+
+                if (amountArray.Length > 1)
+                {
+                    int cents = Convert.ToInt32(input.Split('.')[1]);
+                    output += " and " + cents.NumberToText() + " cents";
+                }
+
+                Console.WriteLine(output);
+            }
+            Console.Read();
+        }
+    }
+}
